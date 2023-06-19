2d594ed31aba5432a12e26831398dd836e2b846c
Bettaht <diegov509@live.com>
2023-06-19 14:06:38 -0600

    Add basic examples

---
+++ src/examples/ContadorComposition.vue
@@ -0,0 +1,25 @@
+<script setup lang = 'ts'>
+import { onMounted, ref } from "vue";//definir variales de estado
+const count = ref(0); 
+
+function increment(){
+    count.value += 1;
+}
+function increase() {
+    count.value -= 1;
+}
+//imprimer mensaje de log al inicio
+onMounted(() => {
+    console.log('El valor inicial del contador es: ' + count.value);
+    
+})
+
+</script>
+
+
+<template>
+    <p>Hola soy la version Composition API</p>
+    <h1>Contador: {{ count }}</h1>
+    <button @click="increment">Incrementar contador</button>
+    <button @click="increase">Disminuir contador</button>
+</template>
